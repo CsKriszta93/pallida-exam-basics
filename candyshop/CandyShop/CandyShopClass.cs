88165164929bb80c77a1afd7bbd0af55aba836d7
CsKriszta93 <kriszta.csere93@gmail.com>
2017-10-06 13:22:14 +0200

    CurrentSugarAmount function created

--- candyshop/CandyShop/CandyShopClass.cs
+++ candyshop/CandyShop/CandyShopClass.cs
@@ -6,18 +6,26 @@ using System.Threading.Tasks;
 
 namespace CandyShop
 {
-    public class CandyShopClass : Sweets
+    public class CandyShopClass
     {
         public List<Sweets> storage = new List<Sweets>();
         public int sugar;
         public int income;
 
-   
-
         public CandyShopClass(int sugar)
         {
             this.sugar = sugar;
         }
+
+        public int CurrentSugarAmount()
+        {
+            foreach (var candy in storage)
+            {
+                sugar -= candy.amountOfSugar;
+            }
+            return sugar;
+        }
+        
 
         public CreateSweets()
         {
